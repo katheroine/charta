f4e1887b0d9f2a2e45b64224882a31afcc03ee78
Katarzyna Krasińska <katheroine@gmail.com>
2013-01-16 12:36:26 +0100

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Charta::Application.config.secret_token = '04ecffe4f2ea3c0fb133c643925f26f095c1a7ab25174c78079fcd18fb99fa4ce31838e138abb4fa15eb7d9d9203611739a7eb118f17861879f4582861969e0f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Charta::Application.config.session_store :cookie_store, key: '_charta_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Charta::Application.config.session_store :active_record_store
