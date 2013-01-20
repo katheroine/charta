17a494bd441059d7e215db663bf42590ef7024ea
Katarzyna Krasińska <katheroine@gmail.com>
2013-01-20 14:54:52 +0100

    admins management process fixed

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,3 +1,30 @@
+require 'digest/sha2'
+
 class Admin < ActiveRecord::Base
-  attr_accessible :bio, :email, :hashed_password, :is_root, :login, :name, :salt, :surname
+  attr_accessible :login, :email, :is_root, :name, :surname, :bio, :hashed_password, :salt, :password, :password_confirmation
+  attr_reader :password
+  
+  validates :password, :confirmation => :true
+  validates :password, :login, :email, :presence => :true
+  
+  private
+  
+  def generate_salt
+		self.salt = self.object_id.to_s + rand.to_s
+  end
+  
+  def Admin.encrypt_password(password, salt)
+		Digest::SHA2.hexdigest(password + "wibble" + salt)
+  end
+  
+  public
+  
+  def password=(password)
+		@password = password
+		
+		if password.present?
+			generate_salt
+			self.hashed_password = self.class.encrypt_password(password, salt)
+		end
+  end
 end
