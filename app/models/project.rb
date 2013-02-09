72f4ffb3870c23d8b2819df0829c0ab283c2d705
Katarzyna Krasińska <katheroine@gmail.com>
2013-02-09 16:02:05 +0100

    project validation state field simplified

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,12 +6,12 @@ class Project < ActiveRecord::Base
   
   validates :name, :presence => :true, :uniqueness => :true, :length => { :maximum => 32 }
   validates :localization, :presence => :true, :length => { :maximum => 64 }
-  validates :state, :presence => :true, :inclusion => { :in => ["at the planning stage", "under construction", "completed"] }
+  validates :state, :presence => :true, :inclusion => STATE
   validates :completion_date, :presence => :true, :if => "state == 'completed'"
   validates :rooms_number, :numericality => { :only_integer => :true }, :allow_nil => :true
   validates :surface_area, :numericality => :true, :allow_nil => :true
-  validates :project_img, :presence => true
-  validates :construction_img, :presence => true, :if => "state == 'under construction' || state == 'completed'"
+  validates :project_img, :presence => :true
+  validates :construction_img, :presence => { :true, :if => "state == 'under construction' || state == 'completed'", :false, :if => "state == 'at the planning stage'" }
   validates :result_img, :presence => :true, :if => "state == 'completed'"
   
   has_attached_file :project_img,
